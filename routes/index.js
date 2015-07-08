300af790c6845c15030ad30c74e0d25a7babe76a
Mark Macdonald <mjemacdonald@gmail.com>
2015-07-08 16:53:59 -0700

    Adds background and basic CSS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'markm.ac' });
+});
+
+router.get('/blog', function(req, res, next) {
+  res.redirect(301, 'http://markm.ac');
 });
 
 module.exports = router;
